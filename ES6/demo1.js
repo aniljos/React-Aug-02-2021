7b8e4fe462c9afa7ccca6a50b2ccdcf337d23076
Anil Joseph <anil.jos@gmail.com>
2021-08-04 11:41:39 +0530

    Day 3 Session 1

---
+++ ES6/demo1.js
@@ -0,0 +1,11 @@
+console.log("demos");
+
+const numbers =[1,2,3,4,5,6,7,8];
+
+
+const sqaures = numbers.map((item, index) => {
+
+    return item * item;
+})
+
+console.log(sqaures);
--- the-awesome-app/src/components/ListProducts.jsx
+++ the-awesome-app/src/components/ListProducts.jsx
@@ -1,28 +1,87 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import './ListProducts.css';
 
-class ListProducts extends Component{
 
 
-    componentDidMount(){
+class ListProducts extends Component {
 
-        const url = "http://localhost:9000/products";
+    state = {
+        data: []
+    }
+    url = "http://localhost:9000/products";
+    componentDidMount() {
+
+        
+        
+        this.fetch();
+    }
+
+    fetch(){
         axios
-            .get(url)
+            .get(this.url)
             .then((response) => {
                 console.log("success", response);
+                this.setState({
+                    data: response.data
+                });
             }, (response) => {
                 console.log("error", response);
             });
+    }
+
+    deleteProduct = async (product) => {
+
+        // axios
+        //     .delete(this.url + "/" + product.id)
+        //     .then(() => {
+        //         alert("deleted");
+        //     }, () => {
+        //         alert("failed to delete");
+        //     });
+
+        //async -await (ES7) ==> Promise
+
+        try {
+            
+            const response = await axios.delete(this.url + "/" + product.id);
+            alert("deleted");
+            this.fetch();
+
+        } catch (error) {
+            alert("failed to delete");
+        }
+        
+
+
+    }
+   
+    renderProducts() {
+
+        return this.state.data.map((item, index) => {
+            return (
+                <div className="product" key={index}>
+                    <p>{item.name}</p>
+                    <p>{item.description}</p>
+                    <p>Price: {item.price}</p>
+                    <div>
+                        <button onClick={() => {this.deleteProduct(item)}}>Delete</button>
+                    </div>
+                </div>
+            );
+        })
 
     }
 
-    render(){
+    render() {
         return (
             <div>
                 <h4>Products</h4>
+                <div style={{display: "flex", flexFlow: "row wrap", justifyContent: "center"}}>
+                    {this.renderProducts()}
+                </div>
             </div>
-        )
+        );
     }
 }
 
